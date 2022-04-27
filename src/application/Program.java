40ed6c86039436075391fdf3ef2fa400cd843890
Carolina Hulse <carolinahulse321@gmail.com>
2022-04-27 16:25:13 -0300

    Primeira classe criada: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
+
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
 	}
 }
