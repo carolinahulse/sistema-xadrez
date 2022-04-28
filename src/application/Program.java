ea6ab12723f99ded7bb258e314d2899a8c715de6
Carolina Hulse <carolinahulse321@gmail.com>
2022-04-28 16:23:00 -0300

    Criando a camada de xadrez e imprimindo o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
-import tabuleiro.Tabuleiro;
+import xadrez.PartidaDeXadrez;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+		UI.printTabuleiro(partidaDeXadrez.getPecas());
 	}
 }
---
+++ src/xadrez/PartidaDeXadrez.java
@@ -0,0 +1,19 @@
+package xadrez;
+import tabuleiro.Tabuleiro;
+public class PartidaDeXadrez {
+	private Tabuleiro tabuleiro;
+	
+	public PartidaDeXadrez() {
+		tabuleiro = new Tabuleiro(8, 8);
+	}
+	
+	public PecaDeXadrez[][] getPecas() {
+		PecaDeXadrez[][] matriz = new PecaDeXadrez[tabuleiro.getLinha()][tabuleiro.getColuna()];
+		for(int i = 0; i < tabuleiro.getLinha(); i++) {
+			for(int j = 0; j < tabuleiro.getColuna(); j++) {
+				matriz[i][j] = (PecaDeXadrez) tabuleiro.peca(i, j);
+			}
+		}
+		return matriz;
+	}
+}
