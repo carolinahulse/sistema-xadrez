ed4bef58214552753106036d205cd5bb281bbc50
Carolina Hulse <carolinahulse321@gmail.com>
2022-04-29 17:44:06 -0300

    Colocando as peças no tabuleiro

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -1,10 +1,14 @@
 package xadrez;
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 public class PartidaDeXadrez {
 	private Tabuleiro tabuleiro;
 	
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		iniciarSetup();
 	}
 	
 	public PecaDeXadrez[][] getPecas() {
@@ -15,5 +19,11 @@ public class PartidaDeXadrez {
 			}
 		}
 		return matriz;
+	}
+	
+	private void iniciarSetup() {
+		tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
+		tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+		tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
 	}
 }
